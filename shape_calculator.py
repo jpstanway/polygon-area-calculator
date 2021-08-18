14a5c9b3936edabc4e24dd251c58aa6b92455f20
Jordan S <jpstanway@gmail.com>
2021-08-17 19:07:27 -0700

    define all methods for both classes

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,4 +1,32 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_picture(self):
+        shape = "*"
+        return shape
+
+    def get_amount_inside(self):
+        return 'square'
 
 
-class Square:
+class Square(Rectangle):
+    def __init__(self, width, height, side):
+        super().__init__(width, height)
+        self.side = side
+
+    def set_side(self, side):
+        self.side = side
